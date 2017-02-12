b76101945400a8a73c8b60155c58c9354994649e
Devang Patel <devang@Devangs-MacBook-Pro.local>
2017-02-12 14:23:55 -0800

    joinpredicate

--- cs133-lab2/src/java/simpledb/JoinPredicate.java
+++ cs133-lab2/src/java/simpledb/JoinPredicate.java
@@ -24,8 +24,16 @@ public class JoinPredicate implements Serializable {
      *            Predicate.Op.LESS_THAN_OR_EQ
      * @see Predicate
      */
+    
+    private int field1;
+    private Predicate.Op op;
+    private int field2;
+    
     public JoinPredicate(int field1, Predicate.Op op, int field2) {
         // some code goes here
+    	this.field1 = field1;
+    	this.field2 = field2;
+    	this.op = op;
     }
 
     /**
@@ -36,24 +44,29 @@ public class JoinPredicate implements Serializable {
      */
     public boolean filter(Tuple t1, Tuple t2) {
         // some code goes here
-        return false;
+    	Field field1T1 = t1.getField(field1);
+    	Field field2T1 = t1.getField(field2);
+    	Field field1T2 = t2.getField(field1);
+    	Field field2T2 = t2.getField(field2);
+    	
+    	return (field1T1.compare(op, field1T2) && field2T1.compare(op, field2T2));
     }
     
     public int getField1()
     {
         // some code goes here
-        return -1;
+        return this.field1;
     }
     
     public int getField2()
     {
         // some code goes here
-        return -1;
+        return this.field2;
     }
     
     public Predicate.Op getOperator()
     {
         // some code goes here
-        return null;
+        return this.op;
     }
 }
